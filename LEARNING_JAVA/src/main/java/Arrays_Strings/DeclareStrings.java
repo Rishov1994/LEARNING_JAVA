d86f104446d5e8071229c6254d6bfc1e442a9a3e
RishovBhatt <rishovmechanical1994@gmail.com>
2024-12-30 21:38:58 +0530

    Adding New Program

---
+++ LEARNING_JAVA/src/main/java/Arrays_Strings/DeclareStrings.java
@@ -0,0 +1,13 @@
+package Arrays_Strings;
+
+public class DeclareStrings {
+    public static void main (String[] args){
+        String myname = "Rishov";
+        String empty = "";
+
+        empty = myname + " Bhattacharjee";
+
+        System.out.println("Full name is: " +empty);
+
+    }
+}
